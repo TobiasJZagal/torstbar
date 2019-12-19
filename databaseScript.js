cdd2c312a78841a69f5c0cece198a0dba75ed538
TobiasJZagal <tobi4680@stud.kea.dk>
2019-12-19 23:35:56 +0100

    final commit

--- databaseScript.js
+++ databaseScript.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
+  document.getElementById("vid").volume = 0;
   get();
   get2();
 }
@@ -24,6 +25,14 @@ function post(item) {
   if (item.acf.category == "about") {
     document.querySelector(".OmHeadline").textContent = item.acf.headline;
     document.querySelector(".OmText").textContent = item.acf.text;
+  } else if (item.acf.category == "special page") {
+    let temp = document.querySelector("#tempSection");
+    let dest = document.querySelector(".eventContainer");
+    klon = temp.cloneNode(true).content;
+    klon.querySelector(".headerEvent").textContent = item.acf.header;
+    klon.querySelector(".textEvent").textContent = item.acf.text;
+    dest.appendChild(klon);
+    console.log(item.acf.header);
   }
 }
 
@@ -38,16 +47,60 @@ function get2() {
 }
 
 let pauseButton = document.getElementById("pause");
+let muteButton = document.getElementById("mute");
 let video = document.getElementById("vid");
+
+video.addEventListener("ended", () => {
+  pauseButton.src = "icons/playW.png";
+  console.log("teeeeeeeeeeeeeest");
+});
 
 pauseButton.addEventListener("click", () => {
   if (video.paused || video.ended) {
     video.play();
-    pauseButton.src = "icons/pause.png";
+    pauseButton.src = "icons/pauseW.png";
   } else {
     video.pause();
-    pauseButton.src = "icons/play.png";
+    pauseButton.src = "icons/playW.png";
   }
+});
+
+muteButton.addEventListener("click", () => {
+  if (video.volume == 0) {
+    video.volume = 1;
+    muteButton.src = "icons/muteW.png";
+  } else {
+    video.volume = 0;
+    muteButton.src = "icons/volumeW.png";
+  }
+});
+
+let about = document.querySelector(".linkOm");
+let events = document.querySelector(".linkEvents");
+let info = document.querySelector(".linkInfo");
+let burger = document.querySelector(".burgerMenu");
+
+burger.addEventListener("click", () => {
+  if (document.querySelector("#burger").checked == false) {
+    burger.textContent = "X";
+  } else {
+    burger.textContent = "☰";
+  }
+  console.log("test");
+});
+
+function toggle() {
+  document.querySelector("#burger").checked = false;
+}
+
+about.addEventListener("click", function() {
+  toggle();
+});
+events.addEventListener("click", function() {
+  toggle();
+});
+info.addEventListener("click", function() {
+  toggle();
 });
 
 let carouselGlide = document.querySelector(".glide");
@@ -55,7 +108,20 @@ let carouselGlide = document.querySelector(".glide");
 new Glide(".glide", {
   type: "carousel",
   startAt: 0,
-  perView: 3
+  perView: 3,
+  autoplay: 4000,
+  hoverpause: true,
+  animationTimingFunc: "cubic-bezier(0.680, -0.550, 0.265, 1.550)",
+  animationDuration: 900,
+  breakpoints: {
+    700: {
+      perView: 2
+    },
+    600: {
+      perView: 2,
+      focusAt: "center"
+    }
+  }
 }).mount();
 /*
 new Glide(".glide", {
